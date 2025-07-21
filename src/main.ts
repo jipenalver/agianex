011e16bd2e54e4c2a7fbebf132cc48c6b55ec19e
jipenalver <jianchris2k14@gmail.com>
2025-07-21 12:38:59 +0800

    chore: move icon import to a dedicated section in main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Icons
+import '@mdi/font/css/materialdesignicons.css'
+
 const app = createApp(App)
 
 const vuetify = createVuetify({
