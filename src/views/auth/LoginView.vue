ee3596e38222219cfc0ee5678a8b12ec2d1544f9
jipenalver <jianchris2k14@gmail.com>
2025-07-21 15:31:12 +0800

    feat: add theme toggle functionality and update logo display in LoginView and RegisterView

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import LogoAgianexDark from '@/assets/logos/logo-agianex-v2-dark.png'
 import LogoAgianex from '@/assets/logos/logo-agianex-v2.png'
 import AppLayout from '@/components/layout/AppLayout.vue'
 import LoginForm from './partials/LoginForm.vue'
 import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
 
 const { xs } = useDisplay()
+
+const theme = ref(localStorage.getItem('theme') ?? 'light')
+
+const onToggleTheme = () => {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
 </script>
 
 <template>
-  <AppLayout>
+  <AppLayout @theme="onToggleTheme">
     <template #content>
       <v-container>
         <v-row class="d-flex justify-center">
@@ -22,7 +30,11 @@ const { xs } = useDisplay()
               elevation="8"
               rounded="lg"
             >
-              <v-img class="mx-auto my-6" max-width="150" :src="LogoAgianex"></v-img>
+              <v-img
+                class="mx-auto my-6"
+                max-width="150"
+                :src="theme === 'light' ? LogoAgianex : LogoAgianexDark"
+              ></v-img>
 
               <LoginForm></LoginForm>
 
