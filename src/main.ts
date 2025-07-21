edae745dd0a0f1cfae278023c8b60139b739b656
jipenalver <jianchris2k14@gmail.com>
2025-07-21 12:53:07 +0800

    feat: integrate custom theme and defaults in Vuetify setup

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,10 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VDateInput } from 'vuetify/labs/VDateInput'
+
+// Custom Vuetify
+import { customDefaults, customTheme } from '@/utils/vuetify/config'
 
 // Icons
 import '@mdi/font/css/materialdesignicons.css'
@@ -24,7 +28,14 @@ createFont()
 const app = createApp(App)
 
 const vuetify = createVuetify({
-  components,
+  theme: {
+    themes: customTheme,
+  },
+  defaults: customDefaults,
+  components: {
+    ...components,
+    VDateInput,
+  },
   directives,
 })
 
