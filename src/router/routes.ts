52c8b341f9d89db87b9fab59b01f15d5df9e4cde
jipenalver <jianchris2k14@gmail.com>
2025-07-21 13:27:37 +0800

    feat: add error handling routes for forbidden and not found pages

---
+++ src/router/routes.ts
@@ -0,0 +1,114 @@
+// import LoginView from '@/views/auth/LoginView.vue'
+// import RegisterView from '@/views/auth/register/RegisterView.vue'
+import ForbiddenView from '@/views/errors/ForbiddenView.vue'
+import NotFoundView from '@/views/errors/NotFoundView.vue'
+// import UserRolesView from '@/views/system/admin/manage-users/UserRolesView.vue'
+// import UsersView from '@/views/system/admin/manage-users/UsersView.vue'
+// import DashboardView from '@/views/system/dashboard/DashboardView.vue'
+// import EmployeesView from '@/views/system/admin/manage-employees/EmployeesView.vue'
+// import RatesBenefitsView from '@/views/system/admin/manage-employees/RatesBenefitsView.vue'
+// import AttendanceView from '@/views/system/admin/manage-attendance/AttendanceView.vue'
+// import LeaveView from '@/views/system/admin/manage-attendance/LeaveView.vue'
+// import OvertimeView from '@/views/system/admin/manage-attendance/OvertimeView.vue'
+// import PayrollView from '@/views/system/admin/manage-payroll/PayrollView.vue'
+// import SettingsView from '@/views/system/settings/SettingsView.vue'
+
+export const routes = [
+  // // Landing
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: LoginView,
+  //   meta: { requiresAuth: false },
+  // },
+  // // {
+  // //   path: '/register',
+  // //   name: 'register',
+  // //   component: RegisterView,
+  // //   meta: { requiresAuth: false },
+  // // },
+
+  // // System
+  // {
+  //   path: '/dashboard',
+  //   name: 'dashboard',
+  //   component: DashboardView,
+  //   meta: { requiresAuth: true, isDefault: true },
+  // },
+  // {
+  //   path: '/settings/:tab?',
+  //   name: 'settings',
+  //   component: SettingsView,
+  //   meta: { requiresAuth: true, isDefault: true },
+  // },
+
+  // // Users
+  // {
+  //   path: '/admin/users/roles',
+  //   name: 'admin-users-roles',
+  //   component: UserRolesView,
+  //   meta: { requiresAuth: true },
+  // },
+  // {
+  //   path: '/admin/users/list',
+  //   name: 'admin-users-list',
+  //   component: UsersView,
+  //   meta: { requiresAuth: true },
+  // },
+
+  // // Employees
+  // {
+  //   path: '/hrms/employees/list',
+  //   name: 'hrms-employees-list',
+  //   component: EmployeesView,
+  //   meta: { requiresAuth: true },
+  // },
+  // {
+  //   path: '/hrms/employees/benefits',
+  //   name: 'hrms-employees-benefits',
+  //   component: RatesBenefitsView,
+  //   meta: { requiresAuth: true },
+  // },
+
+  // // Attendance
+  // {
+  //   path: '/hrms/attendance/list',
+  //   name: 'hrms-attendance-list',
+  //   component: AttendanceView,
+  //   meta: { requiresAuth: true },
+  // },
+  // {
+  //   path: '/hrms/attendance/leave',
+  //   name: 'hrms-attendance-leave',
+  //   component: LeaveView,
+  //   meta: { requiresAuth: true },
+  // },
+  // {
+  //   path: '/hrms/attendance/overtime',
+  //   name: 'hrms-attendance-overtime',
+  //   component: OvertimeView,
+  //   meta: { requiresAuth: true },
+  // },
+
+  // // Payroll
+  // {
+  //   path: '/hrms/payroll/list',
+  //   name: 'hrms-payroll-list',
+  //   component: PayrollView,
+  //   meta: { requiresAuth: true },
+  // },
+
+  // Errors Pages
+  {
+    path: '/forbidden',
+    name: 'forbidden',
+    component: ForbiddenView,
+    meta: { isDefault: true },
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: NotFoundView,
+    meta: { isDefault: true },
+  },
+]
