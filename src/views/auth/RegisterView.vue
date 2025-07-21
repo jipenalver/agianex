5d638d2c241a0aa23cc01238da24590f59eaf860
jipenalver <jianchris2k14@gmail.com>
2025-07-21 14:52:10 +0800

    fix: adjust header title styling order in LoginView component

---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import LogoAgianex from '@/assets/logos/logo-agianex-v2.png'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import RegisterForm from './partials/RegisterForm.vue'
+import { useDisplay } from 'vuetify'
+
+const { xs } = useDisplay()
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <v-container>
+        <v-row class="d-flex justify-center">
+          <v-col cols="12" xl="4" lg="5" sm="8">
+            <RouterLink class="text-decoration-none mt-16" to="/">
+              <h1 class="text-h2 font-weight-black text-primary text-center">AGIANEX</h1>
+            </RouterLink>
+
+            <v-card
+              class="mx-auto pa-8 mt-4 border-md border-solid border-opacity-100 border-primary"
+              elevation="8"
+              rounded="lg"
+            >
+              <v-img class="mx-auto my-6" max-width="150" :src="LogoAgianex"></v-img>
+
+              <RegisterForm></RegisterForm>
+
+              <v-card-text class="text-center">
+                Already have an account?
+                <v-spacer v-if="xs"></v-spacer>
+                <RouterLink class="ms-2 text-primary" to="/register">
+                  Sign In <v-icon icon="mdi-chevron-left"></v-icon>
+                </RouterLink>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </AppLayout>
+</template>
