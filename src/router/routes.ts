7051fbf834db12d337ab89f0efd6b07242a75c8f
jipenalver <jianchris2k14@gmail.com>
2025-07-21 14:58:11 +0800

    feat: restore DashboardView and SettingsView routes in router configuration

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,16 +3,16 @@ import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import ForbiddenView from '@/views/errors/ForbiddenView.vue'
 import NotFoundView from '@/views/errors/NotFoundView.vue'
+import DashboardView from '@/views/system/dashboard/DashboardView.vue'
+import SettingsView from '@/views/system/settings/SettingsView.vue'
 // import UserRolesView from '@/views/system/admin/manage-users/UserRolesView.vue'
 // import UsersView from '@/views/system/admin/manage-users/UsersView.vue'
-// import DashboardView from '@/views/system/dashboard/DashboardView.vue'
 // import EmployeesView from '@/views/system/admin/manage-employees/EmployeesView.vue'
 // import RatesBenefitsView from '@/views/system/admin/manage-employees/RatesBenefitsView.vue'
 // import AttendanceView from '@/views/system/admin/manage-attendance/AttendanceView.vue'
 // import LeaveView from '@/views/system/admin/manage-attendance/LeaveView.vue'
 // import OvertimeView from '@/views/system/admin/manage-attendance/OvertimeView.vue'
 // import PayrollView from '@/views/system/admin/manage-payroll/PayrollView.vue'
-import SettingsView from '@/views/system/settings/SettingsView.vue'
 
 export const routes = [
   // Landing
@@ -38,12 +38,12 @@ export const routes = [
   },
 
   // System
-  // {
-  //   path: '/dashboard',
-  //   name: 'dashboard',
-  //   component: DashboardView,
-  //   meta: { requiresAuth: true, isDefault: true },
-  // },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true, isDefault: true },
+  },
   {
     path: '/settings/:tab?',
     name: 'settings',
