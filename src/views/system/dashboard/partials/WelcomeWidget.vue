b1ecf3ca2b4996a565292580473e15718c58a7f8
jipenalver <jianchris2k14@gmail.com>
2025-07-22 12:43:40 +0800

    feat: Update WelcomeWidget content and image size for improved user experience

--- src/views/system/dashboard/partials/WelcomeWidget.vue
+++ src/views/system/dashboard/partials/WelcomeWidget.vue
@@ -24,7 +24,14 @@ const authUserStore = useAuthUserStore()
             </b>
           </h2>
 
-          <p class="mt-4">
+          <p class="mt-3 text-justify">
+            Agianex is a comprehensive road infrastructure reporting platform that enables citizens
+            and authorities to report, track, and manage road conditions and infrastructure issues.
+            The application features Google Maps for precise location mapping of road incidents and
+            infrastructure problems.
+          </p>
+
+          <p class="mt-3 text-justify">
             We’re excited to have you onboard. If you encounter any issues or have any questions
             about the system, feel free to reach out the technical support,
             <b>jdpenalver@carsu.edu.ph</b>. We're here to support you every step of the way.
@@ -32,7 +39,7 @@ const authUserStore = useAuthUserStore()
         </v-col>
 
         <v-col class="order-1 order-sm-2" cols="12" lg="4" sm="3">
-          <v-img class="mx-auto" max-width="200" :src="MiscWelcomeJohn"></v-img>
+          <v-img class="mx-auto" max-width="225" :src="MiscWelcomeJohn"></v-img>
         </v-col>
       </v-row>
     </v-card-text>
