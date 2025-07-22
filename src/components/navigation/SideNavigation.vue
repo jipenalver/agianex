8b27ae9f042533ab4416dcafc80acda481393554
jipenalver <jianchris2k14@gmail.com>
2025-07-22 09:27:55 +0800

    feat: enhance SideNavigation with additional admin menu items and sub-navigation

--- src/components/navigation/SideNavigation.vue
+++ src/components/navigation/SideNavigation.vue
@@ -4,6 +4,8 @@ import {
   type SubNavigation,
   adminNav,
   adminItemsNav1,
+  adminItemsNav2,
+  adminItemsNav3,
   settingsItemsNav,
 } from './sideNavigation'
 import { useAuthUserStore } from '@/stores/authUser'
@@ -21,13 +23,19 @@ const authUserStore = useAuthUserStore()
 const noAccessPages = ref<string[]>([])
 const mainNav = ref<MainNavigation[] | SubNavigation[]>([])
 const editableItemsNav1 = ref<SubNavigation[]>([...adminItemsNav1])
+const editableItemsNav2 = ref<SubNavigation[]>([...adminItemsNav2])
+const editableItemsNav3 = ref<SubNavigation[]>([...adminItemsNav3])
 
 onMounted(() => {
   mainNav.value = adminNav
 
   if (authUserStore.userRole === 'Super Administrator') return
 
-  const menuItems = [{ items: editableItemsNav1, title: adminNav[0][0] }]
+  const menuItems = [
+    { items: editableItemsNav1, title: adminNav[0][0] },
+    { items: editableItemsNav2, title: adminNav[1][0] },
+    { items: editableItemsNav3, title: adminNav[2][0] },
+  ]
 
   menuItems.forEach(({ items, title }) => {
     if (!items.value) return
@@ -95,15 +103,41 @@ onMounted(() => {
             </template>
           </v-list-item>
         </template>
-      </v-list-group>
 
-      <v-divider></v-divider>
-
-      <v-list-item prepend-icon="mdi-chart-line" to="/reports" color="primary" variant="flat" slim>
-        <template #title>
-          <span class="font-weight-black"> Reports </span>
+        <template v-if="mainNav[1] && title === mainNav[1][0]">
+          <v-list-item
+            v-for="([title, icon, subtitle, to], i) in editableItemsNav1"
+            :key="i"
+            :prepend-icon="icon"
+            :subtitle="subtitle ?? undefined"
+            :to="to ?? undefined"
+            color="primary"
+            variant="flat"
+            slim
+          >
+            <template #title>
+              <span class="font-weight-black"> {{ title }} </span>
+            </template>
+          </v-list-item>
         </template>
-      </v-list-item>
+
+        <template v-if="mainNav[2] && title === mainNav[2][0]">
+          <v-list-item
+            v-for="([title, icon, subtitle, to], i) in editableItemsNav1"
+            :key="i"
+            :prepend-icon="icon"
+            :subtitle="subtitle ?? undefined"
+            :to="to ?? undefined"
+            color="primary"
+            variant="flat"
+            slim
+          >
+            <template #title>
+              <span class="font-weight-black"> {{ title }} </span>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list-group>
 
       <v-divider></v-divider>
 
