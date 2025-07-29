63ee83cb04e626fb0f06b2172eb49ea3cf879252
jipenalver <jianchris2k14@gmail.com>
2025-07-29 18:18:43 +0800

    fix: correct user role condition in WelcomeWidget for displaying support message

--- src/views/system/dashboard/partials/WelcomeWidget.vue
+++ src/views/system/dashboard/partials/WelcomeWidget.vue
@@ -24,14 +24,14 @@ const authUserStore = useAuthUserStore()
             </b>
           </h2>
 
-          <p v-if="authUserStore.userRole === 'User'" class="mt-3 text-justify">
+          <p class="mt-3 text-justify">
             <strong>Agianex</strong> is a comprehensive road infrastructure reporting platform that
             enables citizens and authorities to report, track, and manage road conditions and
             infrastructure issues. The application features Google Maps for precise location mapping
             of road incidents and infrastructure problems.
           </p>
 
-          <p v-if="authUserStore.userRole !== 'User'" class="mt-3 text-justify">
+          <p v-if="authUserStore.userRole === 'User'" class="mt-3 text-justify">
             We’re excited to have you onboard. If you encounter any issues or have any questions
             about the system, feel free to reach out the technical support,
             <a class="font-weight-bold" href="mailto:jdpenalver@carsu.edu.ph">
