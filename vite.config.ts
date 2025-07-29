1de8fb527378693bf9ca7482e25328b401934ae9
jipenalver <jianchris2k14@gmail.com>
2025-07-29 13:08:11 +0800

    fix: remove duplicate import of vueDevTools in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
